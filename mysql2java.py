faf9100cf82e7d80645f515637f47ff7cade2b12
HuangJinAmm <422837038@qq.com>
2020-06-10 18:03:07 +0800

    ss

--- mysql2java.py
+++ mysql2java.py
@@ -6,26 +6,18 @@ pswd = "plat%123"
 
 def do():
     mysql_conn = pymysql.connect(host= '192.168.0.250', port= 3306, user= usr, password= pswd, db= 'plat')
-    sql = '''SELECT 
-    COLUMN_NAME , 
-    COLUMN_TYPE , 
-    DATA_TYPE , 
-    CHARACTER_MAXIMUM_LENGTH , 
-    IS_NULLABLE , 
-    COLUMN_DEFAULT , 
-    COLUMN_COMMENT ,
-FROM
- INFORMATION_SCHEMA.COLUMNS
-where
--- developerclub为数据库名称，到时候只需要修改成你要导出表结构的数据库即可
--- table_schema ='developerclub'
--- AND
--- article为表名，到时候换成你要导出的表的名称
--- 如果不写的话，默认会查询出所有表中的数据，这样可能就分不清到底哪些字段是哪张表中的了，所以还是建议写上要导出的名名称
-table_name  = 'tc_company_info'
-'''
+    sql = "SELECT\
+    COLUMN_NAME ,\
+    COLUMN_TYPE ,\
+    DATA_TYPE ,\
+    CHARACTER_MAXIMUM_LENGTH ,\
+    IS_NULLABLE ,\
+    COLUMN_DEFAULT ,\
+    COLUMN_COMMENT \
+FROM INFORMATION_SCHEMA.COLUMNS where table_name  = 'tc_company_info'"
+    # tabel_sql = "select table_name from information_schema.tables where table_schema='plat'"
     ret = do_query(mysql_conn,sql)
-    mysql_conn.close()
+    # retable = do_query(mysql_conn,tabel_sql)
 
 def do_query(conn,sql):
     try:
@@ -36,7 +28,6 @@ def do_query(conn,sql):
                 print(result_one)
     except Exception as e:
         print(e)
-    return select_result
 
 if __name__ == "__main__":
     do()
